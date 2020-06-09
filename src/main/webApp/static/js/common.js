2f8da6f78a98369583863d99468efa58b3c99bf0
linka39 <1203440758@qq.com>
2020-06-09 14:58:53 +0800

    v1.010 1、新增common.js，实现图片等比例缩放功能

---
+++ src/main/webApp/static/js/common.js
@@ -0,0 +1,28 @@
+
+function ResizeImages()
+{
+   var myimg,oldwidth,oldheight;
+   var maxwidth=800;
+   var maxheight=1000
+   var imgs = document.getElementById('content').getElementsByTagName('img');   //濡傛灉浣犲畾涔夌殑id涓嶆槸article锛岃淇敼姝ゅ
+
+   for(i=0;i<imgs.length;i++){
+     myimg = imgs[i];
+     if(myimg.width > myimg.height)
+     {
+         if(myimg.width > maxwidth)
+         {
+            oldwidth = myimg.width;
+            myimg.height = myimg.height * (maxwidth/oldwidth);
+            myimg.width = maxwidth;
+         }
+     }else{
+         if(myimg.height > maxheight)
+         {
+            oldheight = myimg.height;
+            myimg.width = myimg.width * (maxheight/oldheight);
+            myimg.height = maxheight;
+         }
+     }
+   }
+}
