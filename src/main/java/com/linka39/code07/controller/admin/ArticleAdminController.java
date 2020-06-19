042f271f928cfbb7b75dc5eac20d4e7ddb7c48bb
linka39 <1203440758@qq.com>
2020-06-19 17:34:33 +0800

    v1.032 1、资源帖子修改页面的实现 2、资源帖子设置热门页面的实现 3、单挑，多条删除资源帖子功能

--- src/main/java/com/linka39/code07/controller/admin/ArticleAdminController.java
+++ src/main/java/com/linka39/code07/controller/admin/ArticleAdminController.java
@@ -5,17 +5,22 @@ import com.linka39.code07.entity.Link;
 import com.linka39.code07.init.InitSystem;
 import com.linka39.code07.service.ArticleService;
 import com.linka39.code07.service.LinkService;
+import com.linka39.code07.util.DateUtil;
+import org.apache.commons.io.FileUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +35,57 @@ public class ArticleAdminController {
     private ArticleService articleService;
     @Autowired
     private InitSystem initSystem;
+    @Value("${articleImageFilePath}")
+    private String articleImageFilePath;
+
+    /**
+     * 修改帖子
+     * @param article
+     * @return
+     */
+    @RequestMapping("/update")
+    @RequiresPermissions(value = {"修改帖子"})//设置权限
+    public ModelAndView update(Article article){
+        Article oldArticle = articleService.get(article.getId());
+        oldArticle.setName(article.getName());
+        oldArticle.setArcType(article.getArcType());
+        oldArticle.setContent(article.getContent());
+        oldArticle.setDownload1(article.getDownload1());
+        oldArticle.setPassword(article.getPassword());
+        oldArticle.setPoints(article.getPoints());
+        if(oldArticle.getState()==2){//审核未通过时，更新Lucene索引
+           //todo 修改Lucene索引，redis缓存中删除这个索引
+        }
+        articleService.save(oldArticle);
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("title","修改帖子成功页面");
+        mav.setViewName("admin/modifyArticleSuccess");
+        return mav;
+    }
+    /**
+     * layedit上传图片
+     * @return
+     */
+    @ResponseBody
+    @RequiresPermissions(value = {"图片上传"})//设置权限
+    @RequestMapping("/uploadImage")
+    public Map<String,Object> uploadImage(MultipartFile file)throws Exception{
+        Map<String,Object> map = new HashMap<>();
+        if(!file.isEmpty()){
+            String fileName=file.getOriginalFilename();//获取原文件名
+            String suffixName= fileName.substring(fileName.lastIndexOf(".")); //获取后缀
+            String newFileName= DateUtil.getCurrentDateStr()+suffixName;    //新名字
+            FileUtils.copyInputStreamToFile(file.getInputStream(),new File(articleImageFilePath+DateUtil.getCurrentDatePath()+newFileName));//根据年月日拷贝到新文件
+            map.put("code",0);
+            map.put("msg","上传成功");
+            Map<String,Object> map2=new HashMap<>();
+            //layui指定上传格式，data为标签中对应的属性
+            map2.put("src","/image/"+DateUtil.getCurrentDatePath()+newFileName);
+            map2.put("title",newFileName);
+            map.put("data",map2);
+        }
+        return map;
+    }
 
     /**
      * 分页查询资源帖子信息
@@ -60,9 +116,24 @@ public class ArticleAdminController {
     @RequiresPermissions(value = {"跳转到帖子审核页面"})//设置权限
     public ModelAndView toReViewArticlePage(@PathVariable(value = "id") Integer id){
         ModelAndView mav = new ModelAndView();
-        mav.addObject("title","修改帖子页面");
+        mav.addObject("title","审核帖子页面");
         mav.addObject("article",articleService.get(id));
         mav.setViewName("admin/reviewArticle");
+        return mav;
+    }
+    /**
+     * 跳转到帖子修改页面
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/toModifyArticlePage/{id}")
+    @RequiresPermissions(value = {"跳转到帖子审核页面"})//设置权限
+    public ModelAndView toModifyArticlePage(@PathVariable(value = "id") Integer id){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("title","修改帖子页面");
+        mav.addObject("article",articleService.get(id));
+        //返回的都为嵌入的iframe页面
+        mav.setViewName("admin/modifyArticle");
         return mav;
     }
 
@@ -86,6 +157,57 @@ public class ArticleAdminController {
             oldArticle.setReason(article.getReason());
         }
         articleService.save(oldArticle);
+        map.put("success",true);
+        return map;
+    }
+    /**
+     * 根据id删除一条
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequiresPermissions(value = {"删除帖子"})//设置权限
+    @RequestMapping("/delete")
+    public Map<String,Object> delete(Integer id)throws Exception{
+        Map<String,Object> map = new HashMap<>();
+        articleService.delete(id);
+        //todo 删除该帖子下的所有评论
+        //todo 删除redis索引
+        //todo 删除索引
+        map.put("success",true);
+        return map;
+    }
+    /**
+     * 多选删除
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequiresPermissions(value = {"删除帖子"})//设置权限
+    @RequestMapping("/deleteSelected")
+    public Map<String,Object> delete(String ids)throws Exception{
+        String[] idStr = ids.split(",");
+        for(int i=0;i<idStr.length;++i){
+            articleService.delete(Integer.parseInt(idStr[i]));
+            //todo 删除该帖子下的所有评论
+            //todo 删除redis索引
+            //todo 删除索引
+        }
+        Map<String,Object> map = new HashMap<>();
+        map.put("success",true);
+        return map;
+    }
+    /**
+     * 修改热门状态
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequiresPermissions(value = {"修改热门状态"})//设置权限
+    @RequestMapping("/updateHotState")
+    public Map<String,Object> updateHotState(Integer id,Boolean hot)throws Exception{
+        Article oldArticle = articleService.get(id);
+        oldArticle.setHot(hot);
+        articleService.save(oldArticle);
+        Map<String,Object> map = new HashMap<>();
         map.put("success",true);
         return map;
     }
